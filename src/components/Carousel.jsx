ed823c39203310bfd99a1060ff0cf51327f1b3aa
Thomas Lopes <thomas.lopes@estudante.ufla.br>
2023-11-30 18:02:54 -0300

    Update Houses Section

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,31 @@
+// components/Carousel.jsx
+import React, { useState } from 'react';
+import '../css/Carousel.css';
+
+const Carousel = ({ slides }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + slides.length) % slides.length);
+  };
+
+  return (
+    <div className="carousel">
+      <button className="arrow left" onClick={prevSlide}>&lt;</button>
+      <div className="slide-container">
+        {slides.map((slide, index) => (
+          <div key={index} className={`slide ${index === currentSlide ? 'active' : ''}`}>
+            <img src={slide.url} alt={`Slide ${index}`} />
+          </div>
+        ))}
+      </div>
+      <button className="arrow right" onClick={nextSlide}>&gt;</button>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/HousesData.jsx
@@ -0,0 +1,24 @@
+// data/housesData.js
+const housesData = [
+    {
+      title: 'Lispectorus Watertonni',
+      description: 'Descrição da Casa 1...',
+      characteristics: ['Animal: Beija-flor', 'Pedra: Rubi', 'Flor: Lírio amarelo', 'Cores: Azul, Dourado e Branco'],
+      images: ['../assets/ramphastidae.png', 'image1_2.jpg', 'image1_3.jpg'],
+    },
+    {
+      title: 'Ramphastidae Qinnis',
+      description: 'Descrição da Casa 2...',
+      characteristics: ['Animal: Tucano', 'Pedra: Topázio Laranja', 'Flor: Girassol', 'Cores: Laranja, Amarelo e Preto'],
+      images: ['image2_1.jpg', 'image2_2.jpg', 'image2_3.jpg'],
+    },
+    {
+      title: 'Electrophorus Liberi',
+      description: 'Descrição da Casa 3...',
+      characteristics: ['Animal: Enguia', 'Pedra: Hematita', 'Flor: Vitória Régia', 'Cores: Cinza, Ciano e Laranja'],
+      images: ['image3_1.jpg', 'image3_2.jpg', 'image3_3.jpg'],
+    },
+  ];
+  
+  export default housesData;
+  
