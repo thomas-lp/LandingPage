7ba3aaff09170ada4ff5fe3aa038ce032751038c
thiagof201014 <63051022+thiagof201014@users.noreply.github.com>
2023-12-03 15:40:32 -0300

    Adicionando eventos

---
+++ src/components/EventsData.jsx
@@ -0,0 +1,29 @@
+const eventsData = [
+    {
+      name: 'Evento de arte digital',
+      image: 'https://picsum.photos/200/300',
+      description: 'Esse foi um evento de arte digital',
+    },
+    {
+      name: 'Evento de desenho',
+      image: 'https://picsum.photos/200/301',
+      description: 'Esse foi um evento de desenho',
+    },
+    {
+      name: 'Evento de pixel art',
+      image: 'https://picsum.photos/200/302',
+      description: 'Esse foi um evento de pixel art',
+    },
+    {
+      name: 'Evento de fotografia',
+      image: 'https://picsum.photos/200/303',
+      description: 'Esse foi um evento de fotografia',
+    },
+    {
+      name: 'Evento de pintura',
+      image: 'https://picsum.photos/200/304',
+      description: 'Esse foi um evento de pintura',
+    }
+  ]
+
+export default eventsData;
--- src/components/EventsSection.jsx
+++ src/components/EventsSection.jsx
@@ -1,12 +1,42 @@
 // components/EventsSection.jsx
 import React from 'react';
+import Slider from "react-slick";
+import eventsData from './EventsData';
 
 const EventsSection = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1
+  };
+
   return (
-    <section id="events">
-      <h2>Eventos</h2>
-      <p>Descubra os eventos realizados pelo Por Toda PArte.</p>
-    </section>
+    <div style={{ width: '65%', margin: 'auto' }}>
+      <div style={{ marginTop: '20px' }}>
+        <Slider {...settings}>
+          {eventsData.map((d) => (
+
+            <div key={d.name} style={{ backgroundColor: 'white', height: '450px', color: 'black', margin: '10px' }}>
+              <div style={{ height: '56px', backgroundColor: 'orange', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px 10px 0 0', margin: '0px 10px 0px 10px' }}>
+                <p style={{ fontSize: '20px', color:'white' }}>{d.name}</p>
+              </div>
+
+              <div style={{backgroundColor: 'papayaWhip', borderRadius: '0px 0px 20px 20px', margin: '0px 10px 0px 10px'}}>
+                <img src={d.image} alt="" style={{ height: '400px', width: '400px', borderRadius: '2%', marginLeft: 'auto', marginRight: 'auto' }} />
+
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '4px', padding: '4px' }}>
+                  <p style={{ textAlign: 'center', color: 'black' }}>{d.description}</p>
+                  <button style={{ backgroundColor: 'orange', color: 'white', fontSize: '16px', padding: '6px', borderRadius: '13px' }}>Saiba Mais</button>
+                </div>
+
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
   );
 };
 
