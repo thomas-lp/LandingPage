7077b49e93b9f359e617ea4dac52bda43d21d0a4
thiagof201014 <63051022+thiagof201014@users.noreply.github.com>
2023-12-03 15:41:28 -0300

    Adicionando imports do slick

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import InstagramSection from './components/InstagramSection';
 import ContactSection from './components/ContactSection';
 import Footer from './components/Footer';
 import './css/App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
