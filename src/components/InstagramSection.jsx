0cd3305f2f41dec12c6a9ca3d87fd0d0da55fbdd
Larissa Reis Silva <larissa.silva31@estudante.ufla.br>
2023-12-04 20:29:06 -0300

    Seção Instagram

--- src/components/InstagramSection.jsx
+++ src/components/InstagramSection.jsx
@@ -1,7 +1,6 @@
 // components/InstagramSection.jsx
 import React from 'react';
 import '../css/InstagramSection.css';
-import InstagramEmbed from 'react-instagram-embed';
 
 const InstagramSection = () => {
   return (
@@ -9,18 +8,35 @@ const InstagramSection = () => {
       <div className="instagram-content">
         <h1>Conheça o trabalho de nossos artistas!</h1>
         <p>
-          Confira as últimas postangens das incríveis artes produzidas nos eventos do grupo. Siga-nos no Instagram para não perder nada!
+          Confira as últimas postagens das incríveis artes produzidas nos eventos do grupo. Siga-nos no Instagram para não perder nada!
         </p>
         <div className="instagram-info">
-            <p>
+          <p>
             Siga-nos no Instagram: <a target="blank" href='https://www.instagram.com/todap.arte/' className="instagram-handle">@todap.arte</a>
-            </p>
-
-            <div class="instagram-embed-container">
-                <iframe src="https://www.instagram.com/todap.arte/" width="320" height="400" frameborder="0" scrolling="no" allowtransparency="true"></iframe>
-            </div>
+          </p>
         </div>
 
+        <div className="instagram-feed">
+          {/* Adicione as imagens manualmente aqui */}
+          <div className="instagram-photo">
+            <img src='https://picsum.photos/200/301' alt="Foto 1" />
+          </div>
+          <div className="instagram-photo">
+            <img src='https://picsum.photos/200/301' alt="Foto 2" />
+          </div>
+          <div className="instagram-photo">
+            <img src='https://picsum.photos/200/301' alt="Foto 3" />
+          </div>
+          <div className="instagram-photo">
+            <img src='https://picsum.photos/200/301' alt="Foto 4" />
+          </div>
+          <div className="instagram-photo">
+            <img src='https://picsum.photos/200/301' alt="Foto 5" />
+          </div>
+          <div className="instagram-photo">
+            <img src='https://picsum.photos/200/301' alt="Foto 6" />
+          </div>
+        </div>
       </div>
     </section>
   );
