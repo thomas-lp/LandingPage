b09c244444c7a8a5d57f22069fbdc24dde9db801
Thiago Freire <thiagof.201014@gmail.com>
2023-12-04 15:31:39 -0300

    Formulario de contato adicionado

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,15 +1,30 @@
 // components/ContactSection.jsx
 import React from 'react';
+import { FaWhatsapp, FaInstagram, FaTwitter } from "react-icons/fa";
+import '../css/ContactSection.css';
 
 const ContactSection = () => {
   return (
     <section id="contact">
-      <h2>Contato</h2>
+      <h1>Contato</h1>
       <p>Fale conosco e participe do Por Toda PArte.</p>
-      {/* Adicione um formulário de contato aqui */}
+      <div>
+        <span className='button-social-media' ><FaWhatsapp/></span>  
+        <span className='button-social-media' ><FaInstagram/></span>
+        <span className='button-social-media' ><FaTwitter/></span><br></br>
+      </div>
+      <h2 className='newsletter'>Newsletter</h2>
+      <p>Inscreva-se em nossa newletter para se manter informado das próximas novidades!</p>
+      
       <form>
-        {/* Campos do formulário */}
+        <label for="name"> Nome: </label>
+        <input type="text" id="name" name="name" placeholder="Digite seu nome" required /><br></br><br></br>
+        <label for="email"> Email: </label>
+        <input type="email" id="email" name="email" placeholder="Digite seu email" required /><br></br>
       </form>
+      
+      <button className='contact-submit' type="submit" id="submit" >Enviar</button>
+      
     </section>
   );
 };
