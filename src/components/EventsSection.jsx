91db2cf561459888c0aca1f9e7d6637332ebf27a
Thiago Freire <thiagof.201014@gmail.com>
2023-12-04 16:40:23 -0300

    css da secao eventos adicionado

--- src/components/EventsSection.jsx
+++ src/components/EventsSection.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import Slider from "react-slick";
 import eventsData from './EventsData';
+import '../css/EventsSection.css';
 
 const EventsSection = () => {
   const settings = {
@@ -13,31 +14,33 @@ const EventsSection = () => {
   };
 
   return (
-    <div section id="events" style={{ width: '65%', margin: 'auto' }}>
-    <div style={{ width: '65%', margin: 'auto' }}>
-      <h1 style={{textAlign:'center'}}>Eventos</h1>
-      <p style={{textAlign:'center'}}> Descubra os eventos realizados pelo Por Toda PArte. </p>
-      <div style={{ marginTop: '20px' }}>
-        <Slider {...settings}>
-          {eventsData.map((d) => (
-
-            <div key={d.name} style={{ backgroundColor: 'white', height: '450px', color: 'black', margin: '10px' }}>
-              <div style={{ height: '56px', backgroundColor: 'orange', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px 10px 0 0', margin: '0px 10px 0px 10px' }}>
-                <p style={{ fontSize: '20px', color:'white' }}>{d.name}</p>
-              </div>
-
-              <div style={{backgroundColor: 'papayaWhip', borderRadius: '0px 0px 20px 20px', margin: '0px 10px 0px 10px'}}>
-                <img src={d.image} alt="" style={{ height: '400px', width: '400px', borderRadius: '2%', marginLeft: 'auto', marginRight: 'auto' }} />
-
-                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: '4px', padding: '4px' }}>
-                  <p style={{ textAlign: 'center', color: 'black' }}>{d.description}</p>
-                  <button style={{ backgroundColor: 'orange', color: 'white', fontSize: '16px', padding: '6px', borderRadius: '13px' }}>Saiba Mais</button>
+    <div section id="events" className='events'>
+      <div className='events'>
+        <div className='centralize'>
+          <h1>Eventos</h1>
+          <p> Descubra os eventos realizados pelo Por Toda PArte. </p>
+        </div>
+        <div>
+          <Slider {...settings}>
+            {eventsData.map((d) => (
+              <div key={d.name} className='card'>
+                <div className='card-top'>
+                  <p className='name'>{d.name}</p>
                 </div>
 
+                <div className='image-card'>
+                  <img className='image' src={d.image} alt="" />
+
+                  <div className='card-bottom'>
+                    <p>{d.description}</p>
+                    <button className='saiba-mais'>Saiba Mais</button>
+                  </div>
+
+                </div>
               </div>
-            </div>
-          ))}
-        </Slider>
+            ))}
+          </Slider>
+        </div>
       </div>
     </div>
   );
