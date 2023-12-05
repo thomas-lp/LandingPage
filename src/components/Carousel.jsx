f08e6658d223b58d45dd0e4105a43209a28e7519
Thomas Lopes <thomas.lopes@estudante.ufla.br>
2023-12-05 14:35:02 -0300

    Correções casas e gerais

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,5 @@
 // components/Carousel.jsx
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Carousel.css';
 
 const Carousel = ({ slides }) => {
@@ -13,17 +13,38 @@ const Carousel = ({ slides }) => {
     setCurrentSlide((prevSlide) => (prevSlide - 1 + slides.length) % slides.length);
   };
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, [currentSlide, slides.length]);
+
+  const handleDotClick = (index) => {
+    setCurrentSlide(index);
+  };
+
   return (
     <div className="carousel">
       <button className="arrow left" onClick={prevSlide}>&lt;</button>
-      <div className="slide-container">
+      <div className="slide-container" style={{ transform: `translateX(-${currentSlide * 100}%)` }}>
         {slides.map((slide, index) => (
-          <div key={index} className={`slide ${index === currentSlide ? 'active' : ''}`}>
-            <img src={slide} alt={`Slide ${index}`} />
+          <div key={index} className="slide">
+            <img src={slide} alt={`Slide ${index}`} style={{ width: '100%' }} />
           </div>
         ))}
       </div>
       <button className="arrow right" onClick={nextSlide}>&gt;</button>
+      <div className="dots-container">
+        {slides.map((_, index) => (
+          <span
+            key={index}
+            className={`dot ${index === currentSlide ? 'active' : ''}`}
+            onClick={() => handleDotClick(index)}
+          ></span>
+        ))}
+      </div>
     </div>
   );
 };
