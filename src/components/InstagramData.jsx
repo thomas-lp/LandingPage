6c0fe103cfbdf1f06b07546a121f8eab24f09f6a
Larissa Reis Silva <larissa.silva31@estudante.ufla.br>
2023-12-05 13:50:15 -0300

    Imagens Instagram

---
+++ src/components/InstagramData.jsx
@@ -0,0 +1,19 @@
+// components/InstagramData.jsx
+import React from 'react';
+import ig1 from '../assets/instagram/ig1.jpeg';
+import ig2 from '../assets/instagram/ig2.jpeg';
+import ig3 from '../assets/instagram/ig3.jpeg';
+import ig4 from '../assets/instagram/ig4.jpeg';
+import ig5 from '../assets/instagram/ig5.jpeg';
+import ig6 from '../assets/instagram/ig6.jpeg';
+
+const instagramData = [
+  { id: 1, src: ig1, alt: 'Instagram Photo 1' },
+  { id: 2, src: ig2, alt: 'Instagram Photo 2' },
+  { id: 3, src: ig3, alt: 'Instagram Photo 3' },
+  { id: 4, src: ig4, alt: 'Instagram Photo 4' },
+  { id: 5, src: ig5, alt: 'Instagram Photo 5' },
+  { id: 6, src: ig6, alt: 'Instagram Photo 6' },
+];
+
+export default instagramData;
